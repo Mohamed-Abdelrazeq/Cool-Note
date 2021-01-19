833256f68a6f21028dac2b98a90ecaf2ef1a4057
mohamedAbdelrazek2000 <mohamedabdelrazek824@gmail.com>
2021-01-19 05:20:15 +0200

    Sqlite Connection Working

--- lib/Models/dbConnection.dart
+++ lib/Models/dbConnection.dart
@@ -7,13 +7,11 @@ import 'package:sqflite/sqflite.dart';
 
 import 'NoteModel.dart';
 
-void main() async {
+Future<Database> dbConnection() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   final Future<Database> database = openDatabase(
-
     join(await getDatabasesPath(), 'notes_database.db'),
-
     onCreate: (db, version) {
       return db.execute(
         "CREATE TABLE notes(id INTEGER PRIMARY KEY, headline TEXT, note TEXT,date TEXT)",
@@ -22,84 +20,86 @@ void main() async {
     version: 1,
   );
 
-  Future<void> insertNote(Note note) async {
-    final Database db = await database;
-
-    await db.insert(
-      'notes',
-      note.toMap(),
-      conflictAlgorithm: ConflictAlgorithm.replace,
-    );
-  }
-
-  Future<List<Note>> notes() async {
-    // Get a reference to the database.
-    final Database db = await database;
-
-    // Query the table for all The Dogs.
-    final List<Map<String, dynamic>> maps = await db.query('notes');
-
-    // Convert the List<Map<String, dynamic> into a List<Dog>.
-    return List.generate(maps.length, (i) {
-      return Note(
-        id: maps[i]['id'],
-        headline: maps[i]['headline'],
-        date: maps[i]['date'],
-        note: maps[i]['note'],
-      );
-    });
-  }
-
-  Future<void> updateNote(Note note) async {
-    // Get a reference to the database.
-    final db = await database;
-
-    // Update the given Dog.
-    await db.update(
-      'notes',
-      note.toMap(),
-      // Ensure that the Dog has a matching id.
-      where: "id = ?",
-      // Pass the Dog's id as a whereArg to prevent SQL injection.
-      whereArgs: [note.id],
-    );
-  }
-
-  Future<void> deleteNote(int id) async {
-    // Get a reference to the database.
-    final db = await database;
-
-    // Remove the Dog from the database.
-    await db.delete(
-      'notes',
-      // Use a `where` clause to delete a specific dog.
-      where: "id = ?",
-      // Pass the Dog's id as a whereArg to prevent SQL injection.
-      whereArgs: [id],
-    );
-  }
-  // // Insert a dog into the database.
-  // await insertNote(fido);
-  //
-  // // Print the list of dogs (only Fido for now).
-  // print(await notes());
-
-  // Update Fido's age and save it to the database.
-  // fido = Note(
-  //   id: fido.id,
-  //   headline: fido.headline,
-  //   note: fido.note,
-  //   date: 'tomorrow'
-  // );
-  //
-  // await updateNote(fido);
-  //
-  // // Print Fido's updated information.
-  // print(await notes());
-  //
-  // Delete Fido from the database.
-  // await deleteNote(fido.id);
-  //
-  // // Print the list of dogs (empty).
-  // print(await notes());
+  return database;
 }
+
+Future<void> insertNote(Note note,Database database) async {
+  final Database db = database;
+
+  await db.insert(
+    'notes',
+    note.toMap(),
+    conflictAlgorithm: ConflictAlgorithm.replace,
+  );
+}
+
+Future<List<Note>> notes(Database database) async {
+  // Get a reference to the database.
+  final Database db = database;
+
+  // Query the table for all The Dogs.
+  final List<Map<String, dynamic>> maps = await db.query('notes');
+
+  // Convert the List<Map<String, dynamic> into a List<Dog>.
+  return List.generate(maps.length, (i) {
+    return Note(
+      id: maps[i]['id'],
+      headline: maps[i]['headline'],
+      date: maps[i]['date'],
+      note: maps[i]['note'],
+    );
+  });
+}
+
+Future<void> updateNote(Note note,Database database) async {
+  // Get a reference to the database.
+  final db = database;
+
+  // Update the given Dog.
+  await db.update(
+    'notes',
+    note.toMap(),
+    // Ensure that the Dog has a matching id.
+    where: "id = ?",
+    // Pass the Dog's id as a whereArg to prevent SQL injection.
+    whereArgs: [note.id],
+  );
+}
+
+Future<void> deleteNote(int id,Database database) async {
+  // Get a reference to the database.
+  final db = database;
+
+  // Remove the Dog from the database.
+  await db.delete(
+    'notes',
+    // Use a `where` clause to delete a specific dog.
+    where: "id = ?",
+    // Pass the Dog's id as a whereArg to prevent SQL injection.
+    whereArgs: [id],
+  );
+}
+// // Insert a dog into the database.
+// await insertNote(fido);
+//
+// // Print the list of dogs (only Fido for now).
+// print(await notes());
+
+// Update Fido's age and save it to the database.
+// fido = Note(
+//   id: fido.id,
+//   headline: fido.headline,
+//   note: fido.note,
+//   date: 'tomorrow'
+// );
+//
+// await updateNote(fido);
+//
+// // Print Fido's updated information.
+// print(await notes());
+//
+// Delete Fido from the database.
+// await deleteNote(fido.id);
+//
+// // Print the list of dogs (empty).
+// print(await notes());
--- lib/Views/Screens/AddNoteScreen.dart
+++ lib/Views/Screens/AddNoteScreen.dart
@@ -1,15 +1,13 @@
 import 'package:coolnote_app/Models/NoteModel.dart';
+import 'package:coolnote_app/Models/dbConnection.dart';
 import 'package:coolnote_app/Views/GeneralWidgets/MultilineTextField.dart';
 import 'package:coolnote_app/Views/GeneralWidgets/MyFloatingActionButton.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 import '../ProjectConstants.dart';
 
-var fido = Note(
-  id: 0,
-  headline: 'test',
-  note: 'working',
-  date: 'today',
-);
+TextEditingController headlineController = TextEditingController();
+TextEditingController noteController = TextEditingController();
 
 class AddNoteScreen extends StatelessWidget {
   @override
@@ -17,109 +15,117 @@ class AddNoteScreen extends StatelessWidget {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
 
-    TextEditingController headlineController = TextEditingController();
-    TextEditingController noteController = TextEditingController();
-
-    return Scaffold(
-      backgroundColor: Color(0xff101010),
-      body: Stack(
-        children: [
-          //TopBar
-          Positioned(
-            top: height * .07,
-            child: Container(
-              width: width,
-              padding: EdgeInsets.symmetric(horizontal: width * .04),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                        color: mainWidget,
-                        borderRadius: BorderRadius.circular(height * .02)),
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Icon(
-                        Icons.arrow_back_ios_outlined,
-                        color: mainText,
-                        size: height * .045,
+    return GestureDetector(
+      onTap: (){
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Scaffold(
+        backgroundColor: Color(0xff101010),
+        body: Stack(
+          children: [
+            //TopBar
+            Positioned(
+              top: height * .07,
+              child: Container(
+                width: width,
+                padding: EdgeInsets.symmetric(horizontal: width * .04),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                          color: mainWidget,
+                          borderRadius: BorderRadius.circular(height * .02)),
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(
+                          Icons.arrow_back_ios_outlined,
+                          color: mainText,
+                          size: height * .045,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    width: width * .05,
-                  ),
-                  Text(
-                    'Notes',
-                    style: TextStyle(
-                        fontSize: height * .065,
-                        color: mainText,
-                        fontWeight: FontWeight.w300),
-                  ),
-                ],
+                    SizedBox(
+                      width: width * .05,
+                    ),
+                    Text(
+                      'Notes',
+                      style: TextStyle(
+                          fontSize: height * .065,
+                          color: mainText,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          //Body
-          Positioned(
-            top: height * .17,
-            left: width * .04,
-            child: Container(
-              width: width * .92,
-              height: height * .83,
-              child: ListView(
-                physics: BouncingScrollPhysics(),
-                children: [
-                  MultilineTextField(
-                    myController: headlineController,
-                    width: width,
-                    height: height,
-                    backgroundColor: Colors.black,
-                    textColor: Colors.white,
-                    hint: 'Headline',
-                    myMaxLines: 3,
-                  ),
-                  SizedBox(
-                    height: height * .02,
-                  ),
-                  MultilineTextField(
-                    myController: noteController,
-                    width: width,
-                    height: height,
-                    backgroundColor: Colors.black,
-                    textColor: Colors.white,
-                    hint: 'What is in your mind?',
-                    myMaxLines: 13,
-                  ),
-                  SizedBox(
-                    height: height * .04,
-                  ),
-                ],
+            //Body
+            Positioned(
+              top: height * .17,
+              left: width * .04,
+              child: Container(
+                width: width * .92,
+                height: height * .83,
+                child: ListView(
+                  physics: BouncingScrollPhysics(),
+                  children: [
+                    MultilineTextField(
+                      myController: headlineController,
+                      width: width,
+                      height: height,
+                      backgroundColor: Colors.black,
+                      textColor: Colors.white,
+                      hint: 'Headline',
+                      myMaxLines: 3,
+                    ),
+                    SizedBox(
+                      height: height * .02,
+                    ),
+                    MultilineTextField(
+                      myController: noteController,
+                      width: width,
+                      height: height,
+                      backgroundColor: Colors.black,
+                      textColor: Colors.white,
+                      hint: 'What is in your mind?',
+                      myMaxLines: 13,
+                    ),
+                    SizedBox(
+                      height: height * .04,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          //FloatingActionButton
-          Positioned(
-            top: height * .88,
-            child: GestureDetector(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: MyFloatingActionButton(
-                height: height,
-                width: width,
-                buttonColor: mainWidget,
-                iconColor: mainText,
-                shadowColor: Colors.grey,
-                icon: Icons.done,
-                text: 'Save Note',
+            //FloatingActionButton
+            Positioned(
+              top: height * .88,
+              child: GestureDetector(
+                onTap: () async {
+                  Database database = await dbConnection();
+                  deleteNote(1, database);
+                  Note note = Note(headline: headlineController.text,date: 'May 21,2020',note: noteController.text);
+                  insertNote(note, database);
+                  List<Note> myNotes = await notes(database);
+                  print(myNotes);
+                  Navigator.pop(context);
+                },
+                child: MyFloatingActionButton(
+                  height: height,
+                  width: width,
+                  buttonColor: mainWidget,
+                  iconColor: mainText,
+                  shadowColor: Colors.grey,
+                  icon: Icons.done,
+                  text: 'Save Note',
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
